1c12939c1fc5200709d81956900d7889c326e10b
KanaAm <amornnakkanathip@gmail.com>
2022-11-17 11:34:30 +0700

    Exercise4

--- 2213111467_Java/src/Exercise4.java
+++ 2213111467_Java/src/Exercise4.java
@@ -1,7 +1,9 @@
 import java.text.*;
 import javax.swing.JOptionPane;
+
 public class Exercise4 {
 	static final double TAX_RATE = 0.0625;
+
 	public static void main(String[] args) {
 		DecimalFormat df = new DecimalFormat("#.#####");
 		DecimalFormat sdf = new DecimalFormat("#.#");
@@ -10,20 +12,25 @@ public class Exercise4 {
 		DecimalFormat one = new DecimalFormat("#.#");
 		DecimalFormat daf = new DecimalFormat("#.#####");
 		// TODO Auto-generated method stub
-		String cdID,cdTitle;
-		cdID = JOptionPane.showInputDialog("This program calculates the total cost of a CD order \nPlease enter the ID of the CD");
+		String cdID, cdTitle;
+		cdID = JOptionPane.showInputDialog(
+				"This program calculates the total cost of a CD order \nPlease enter the ID of the CD");
 		cdTitle = JOptionPane.showInputDialog("Please enter the title of the CD");
 		String cdQuantity;
-		double cdSubtotal,cdTotal;
-		String cdPrice;//double
+		double cdSubtotal, cdTotal;
+		String cdPrice;// double
 		cdPrice = JOptionPane.showInputDialog("Please enter the price of the CD in U.S. dollars");
 		double DcdPrice = Double.parseDouble(cdPrice);
 		cdQuantity = JOptionPane.showInputDialog("Please enter the quantity to be purchase");
 		double DcdQuantity = Double.parseDouble(cdQuantity);
-		cdSubtotal = DcdPrice*DcdQuantity;
-		
+		cdSubtotal = DcdPrice * DcdQuantity;
+
 		cdTotal = cdSubtotal * (1 + TAX_RATE);
-		JOptionPane.showMessageDialog(null, "Summary of the transacton\n\n"+"CD ID:"+cdID+"\nCD Title:"+cdTitle+"\nCD Unit Price:"+f2.format(DcdPrice)+"\nCD Quantity:"+one.format(DcdQuantity)+"\n\nSubtotal:"+"$"+sdf.format(cdSubtotal)+"\nTax rate:"+(TAX_RATE*100)+"%"+"\nTotal:"+"$"+df.format(cdTotal)+"\n\nEnd of Program");
+		JOptionPane.showMessageDialog(null,
+				"Summary of the transacton\n\n" + "CD ID:" + cdID + "\nCD Title:" + cdTitle + "\nCD Unit Price:"
+						+ f2.format(DcdPrice) + "\nCD Quantity:" + one.format(DcdQuantity) + "\n\nSubtotal:" + "$"
+						+ sdf.format(cdSubtotal) + "\nTax rate:" + (TAX_RATE * 100) + "%" + "\nTotal:" + "$"
+						+ df.format(cdTotal) + "\n\nEnd of Program");
 	}
 
 }
