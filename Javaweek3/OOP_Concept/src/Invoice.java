981ac12ec554be80fd8eaa1dcb16c8ee39f20d54
KanaAm <amornnakkanathip@gmail.com>
2023-01-26 12:22:34 +0700

    26-1-2023

--- Javaweek3/OOP_Concept/src/Invoice.java
+++ Javaweek3/OOP_Concept/src/Invoice.java
@@ -3,7 +3,26 @@ public class Invoice {
 	private int id;
 	private Customer customer;
 	private double amount;
-	public Invoice(int id,Customer customer,) {
-		
+	public Invoice(int id,Customer customer,double amount) {
+		id=0;
+		amount=0;
+	}
+	public int getID() {
+		return this.id;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public int getCustomerID() {
+		return id;
+	}
+	public String getCustomerName() {
+		return customer.getName();
 	}
 }
